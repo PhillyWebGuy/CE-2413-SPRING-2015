0a68201a46175aa6218d1e84233f8873033a8e82
Ed Hertzog <edhertzog@gmail.com>
2015-02-10 23:08:05 +0000

    Class 2 Presentations

---
+++ class-2/js/homework-1.js
@@ -0,0 +1,17 @@
+/*<ol>
+1) Set one variable to your first name
+2) Set second variable to your last name
+3) Set a third variable to the concatenated
+return value of "My name is " + #1 + #2
+4) Write the value to the browswer document
+*/
+
+var firstName = "Ed";
+var lastName = "Hertzog";
+var fullName = firstName + " " + lastName;
+
+//call your function to write the string
+document.write("My name is " + fullName);
+
+
+
